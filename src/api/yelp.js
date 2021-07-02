e851eb99c9a7e13c11b9609ae6a594fef89a5d17
dasecure <45675441+dasecure@users.noreply.github.com>
2021-07-02 12:02:39 -0700

    Delete src/api directory

--- src/api/yelp.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer UUry3Qxz9_SuU1hpi1uC4yAkxSlHVr-TENpQCDBzvBy1JoPFMLfDF9v2Xdn5bUqDJDo7J8m_MuqWjFzP8reijIfPIi5OiUriWDufFgOQYoyNSUyg06owpmtQaz7eYHYx'        
-    }
-})
